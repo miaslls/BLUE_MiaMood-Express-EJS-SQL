f38b430fe30e882307f4b3c85c309c660a65d9e5
miaslls <97785569+miaslls@users.noreply.github.com>
2022-04-01 15:21:16 -0300

    BKP before PUT/DELETE

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,4 +1,4 @@
-'use stric';
+'use strict';
 
 const dateList = document.getElementsByClassName('date');
 const timeList = document.getElementsByClassName('time');
@@ -28,18 +28,3 @@ for (let time of timeList) {
     time.innerText = hours.toString().padStart(2,'0') + ':' + minutes + amPm;
 
 }
-
-// const testList = [{date: '2022-03-01', time: '10:10:00'}, {date: '2022-03-01', time: '10:15:00'}, {date: '2022-03-01', time: '10:20:00'}, {date: '2022-03-02', time: '10:11:00'}, {date: '2022-04-01', time: '10:17:00'}, {date: '2022-03-02', time: '10:05:00'}];
-
-// testList.sort((a, b) => a.time - b.time);
-// testList.sort((a, b) => a.date - b.date);
-
-// console.log(testList);
-
-// const testList = [{timestamp: 202203011010}, {timestamp: 202203011015}, {timestamp: 202203011020}, {timestamp: 202203021011}, {timestamp: 202204011017}, {timestamp: 202203021005}]
-
-// testList.sort((a, b) => b.timestamp - a.timestamp);
-
-// console.log(testList);
-
-
