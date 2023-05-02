7b726f3b67c1d46a9e1942df673f71d2c5195170
miaslls <97785569+miaslls@users.noreply.github.com>
2023-05-02 14:48:36 -0300

    .

--- src/models/Mood.js
+++ src/models/Mood.js
@@ -1,50 +1,53 @@
-'use strict';
+"use strict";
 
+const { Sequelize } = require("sequelize");
+const connection = require("../database/db");
 
-const { Sequelize } = require('sequelize')
-const connection = require('../database/db');
-
-const Mood = connection.define('mood', {
+const Mood = connection.define(
+  "mood",
+  {
     mood_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false
+      type: Sequelize.INTEGER,
+      allowNull: false,
     },
     icon: {
-        type: Sequelize.STRING,
-        allowNull: false
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     text: {
-        type: Sequelize.STRING,
-        allowNull: true
+      type: Sequelize.STRING,
+      allowNull: true,
     },
     date: {
-        type: Sequelize.DATEONLY,
-        allowNull: false
+      type: Sequelize.DATEONLY,
+      allowNull: false,
     },
     time: {
-        type: Sequelize.STRING,
-        allowNull: false
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     timestamp: {
-        type: Sequelize.BIGINT,
-        allowNull: false
+      type: Sequelize.BIGINT,
+      allowNull: false,
     },
     createdat: {
-        type: Sequelize.BIGINT,
-        allowNull: false,
-        primaryKey: true
-    }
-}, {
+      type: Sequelize.BIGINT,
+      allowNull: false,
+      primaryKey: true,
+    },
+  },
+  {
     freezeTableName: true,
     timestamps: false,
     createdAt: false,
-    updatedAt: false
-});
+    updatedAt: false,
+  }
+);
 
-// const initTable = async () => {
-//     await Mood.sync();
-// }
+const initTable = async () => {
+  await Mood.sync();
+};
 
-// initTable();
+initTable();
 
 module.exports = Mood;
